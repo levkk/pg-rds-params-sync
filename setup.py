0fa30771c177f0835883d8c90eaae46b2ca48b17
Lev Kokotov <lev.kokotov@instacart.com>
2020-05-29 13:27:50 -0700

    fix setup script

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setuptools.setup(
     python_requires='>=3',
     entry_points={
         'console_scripts': [
-            'pgrdsparamsync = pgrdsparamsync:entrypoint',
+            'pgrdsparamsync = rdsparamsync:entrypoint',
         ]
     },
 )
